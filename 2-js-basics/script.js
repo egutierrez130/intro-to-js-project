9d409a4ee0fc3834a01bf5fdffe168f57eef9b63
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-02-18 14:50:26 -0800

    Learned about different JS Datatypes and Variables

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -1,1 +1,28 @@
-console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+
+
+/* Primitive JavaScript Data Types
+
+1. Number: Floating point numbers, for decimals and integers
+2. String: Sequence of characters, used for text
+3. Boolean: Logical data type that can only be true or false
+4. Undefined: Data type of a variable that does not have a value yet.
+5. Null: Also means 'non-existant'
+
+JavaScript has dynamic typing: data types are automatically assigned to variables
+*/
