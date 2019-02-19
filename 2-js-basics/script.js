f62dd10be7f3c9f09ddef69dca195721420261ed
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-02-18 17:20:59 -0800

    Learned about Operator Precedence

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -1,28 +1,113 @@
-var firstName = 'John';
-console.log(firstName);
-
-var lastName = 'Smith';
-var age = 28;
-
-var fullAge = true;
-console.log(fullAge);
-
-var job;
-console.log(job);
-
-job = 'Teacher';
-console.log(job);
-
-
-
-
-/* Primitive JavaScript Data Types
-
-1. Number: Floating point numbers, for decimals and integers
-2. String: Sequence of characters, used for text
-3. Boolean: Logical data type that can only be true or false
-4. Undefined: Data type of a variable that does not have a value yet.
-5. Null: Also means 'non-existant'
-
-JavaScript has dynamic typing: data types are automatically assigned to variables
+/************************
+* Variables and type coerecion
 */
+/*var firstName = 'John';
+var age = 28;
+// Type coercion
+console.log(firstName + ' ' + age);
+
+// here we declare multiple variables on the same line
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty-eight';
+job = 'driver';
+
+// "alert" is like a pop-up on the browser window
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// "prompt" is a Pop-up on browser window that requires user feedback
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+
+
+/************************
+* Basic Operators
+*/
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn,yearMark);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical operator
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
+
+
+/****************************
+* Operator Precedence
+*/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple Operators
+// Doing this calculation all in one line
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average); // 32
+
+// Multiple Assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6;
+console.log(x, y);
+
+
+// More Operators
+//x = x * 2; // is the same as
+x *= 2;
+console.log(x); // 52
+x += 10;
+console.log(x); // 62
+x++;
+console.log(x); // 63
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
