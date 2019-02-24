b8068fc2d9d476ba30e9b23f0e4f4d1171a3a3f3
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-02-23 18:28:23 -0800

    Successfully Completed Coding Challenge 2

---
+++ coding-challenge-2/script.js
@@ -0,0 +1,31 @@
+// Calculating John and Mike's average score for each team
+var gameOneJohn, gameTwoJohn, gameThreeJohn, totalPointsJohn, averageTeamPointsJohn, teamJohn;
+var gameOneMike, gameTwoMike, gameThreeMike, totalPointsMike, averageTeamPointsMike, teamMike;
+
+//John's Team Scores and Average Points Per Game
+gameOneJohn = 89;
+gameTwoJohn = 120;
+gameThreeJohn = 103;
+totalPointsJohn = gameOneJohn + gameTwoJohn + gameThreeJohn;
+averageTeamPointsJohn = totalPointsJohn / 3;
+console.log('John\'s Team averaged ' + averageTeamPointsJohn + ' points per game.');
+var teamJohn = averageTeamPointsJohn;
+
+//Mike's Team Scores and Average Points Per Game
+gameOneMike = 116;
+gameTwoMike = 94;
+gameThreeMike = 123;
+totalPointsMike = gameOneMike + gameTwoMike + gameThreeMike;
+averageTeamPointsMike = totalPointsMike / 3;
+console.log('Mike\'s Team averaged ' + averageTeamPointsMike + ' points per game.');
+var teamMike = averageTeamPointsMike;
+
+// Team With Highest Scoring Average Per Game
+if (teamJohn > teamMike) {
+  console.log('John\'s Team is the winner averaging ' + averageTeamPointsJohn + ' points per basketball bame.');
+} else if (teamMike > teamJohn) {
+  console.log('Mike\'s Team is the winner averaging ' + averageTeamPointsMike + ' points per basketball bame.');
+} else {
+  console.log('We have a Draw with both teams averaging the same points per basketball game.');
+}
+// Mike's team should be the winner
