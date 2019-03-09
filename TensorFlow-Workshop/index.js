5242e2905b3da4b71983b3506d9f376b43e72fcb
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-03-09 11:57:32 -0800

    TensorFlow.js Workshop at IWD Bitwise.

---
+++ TensorFlow-Workshop/index.js
@@ -0,0 +1,18 @@
+let net;
+const webcamElement = document.getElementById('webcam');
+
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
+}
+
+app();
