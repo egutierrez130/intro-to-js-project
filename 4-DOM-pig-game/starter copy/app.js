62cf17e21076889b8fcb2a472427b958982c1ee5
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-03-24 21:47:22 -0700

    Completed Lesson on DRY Principle. Learned how to Access the classList that elements hold.

--- 4-DOM-pig-game/starter copy/app.js
+++ 4-DOM-pig-game/starter copy/app.js
@@ -58,28 +58,57 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
   } else {
     //Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    // removing and adding classes below
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-
-    document.querySelector('.dice').style.display = 'none';
-
+    nextPlayer();
   }
 
 });
 
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add CURRENT score to GLOBAL score
+  scores[activePlayer] += roundScore;
+
+  // Update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  // Check if player won the game
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#score-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    // Next player
+    nextPlayer();
+  }
+});
 
 
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  /*same as above code except, code above is cleaner.
+  if(activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+      activePlayer = 0;
+  }
+  */
+
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // removing and adding classes below
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
 
 
 
