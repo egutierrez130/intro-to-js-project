408dd7c5ca70c110170b5d3816d2c4a1b864f0fa
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-03-27 23:40:23 -0700

    Completed Coding Challenge 6: Added more functions to Dice Game, Changed the Rules, Added another Dice, Modified IF statements.

--- coding-challenge-6/pig-game-starter-challenge-6/challenge-1-app.js
+++ coding-challenge-6/pig-game-starter-challenge-6/challenge-1-app.js
@@ -9,11 +9,10 @@ GAME RULES:
 */
 // Coding Challenge 6
 /*
-YOUR 3 CHALLENGES
-Change the game to follow these rules:
+YOUR 3 CHALLENGES**************************
 
 1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+2. Add an input field to the HTML where playe rs can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 
@@ -21,31 +20,37 @@ var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-// Event Listeners
-// We need to pass in two arguments: 1.event types 2.function that will happen once the event happens. The name of the of the function becomes "call-back function" because its a function that is called by another function.
+var lastDice;
 
-// this was an example for using a built in function in the addEventListener //document.querySelector('.btn-roll').addEventListener('click', btn);
-// function btn(){
-//   // do something there
-// }
-
-// document.querySelector('.btn-roll').addEventListener('click', btn);
-// or you can pass in an anonymous function.
-// the anonymous function will not have a name.
-// can not be reused.
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if(gamePlaying) {
     // 1. Random number
-    var dice = Math.floor(Math.random() * 6)+ 1;
+    var dice1 = Math.floor(Math.random() * 6)+ 1;
+    var dice2 = Math.floor(Math.random() * 6)+ 1;
 
     // 2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
-
+    // 3. Update the round score IF the rolled number
+    if (dice1 !== 1 && dice2 !== 1) {
+      //Add Scores
+      roundScore += dice1 + dice2;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      //Next player
+      nextPlayer();
+    }
+/*
     // 3. Update the round score IF the rolled number was NOT a 1.
-    if (dice !== 1) {
+    if(dice === 6 && lastDice === 6) {
+      // Player loses score
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = '0';
+      nextPlayer();
+    } else if (dice !== 1) {
       //Add Scores
       roundScore += dice;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -53,6 +58,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
       //Next player
       nextPlayer();
     }
+
+    lastDice = dice;
+*/
   }
 });
 
@@ -65,10 +73,24 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    // CHALLENGE 2************************
+    var input = document.querySelector('.final-score').value;
+    var winningScore;
+
+    // Undefined, 0, null or empty string "" are COERCED to false
+    // Anythig else is COERCED to true
+    if(input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
+
     // Check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gamePlaying = false;
@@ -84,14 +106,6 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
-  /*same as above code except, code above is cleaner.
-  if(activePlayer === 0) {
-    activePlayer = 1;
-  } else {
-      activePlayer = 0;
-  }
-  */
-
 
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
@@ -99,11 +113,8 @@ function nextPlayer() {
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  // removing and adding classes below
-  // document.querySelector('.player-0-panel').classList.remove('active');
-  // document.querySelector('.player-1-panel').classList.add('active');
-
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 // Adding functionality to the "New Game Button"
@@ -116,11 +127,8 @@ function init() {
   activePlayer = 0;
   gamePlaying = true;
 
-  // using the querySelector to change the CSS.
-  // we use 'style' to call the CSS.
-  // we use 'display' to call the CSS property.
-  // we use 'none' to call the CSS value.
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 
   // "getElementById" is a little faster than "querySelector"
   document.getElementById('score-0').textContent = '0';
@@ -135,14 +143,3 @@ function init() {
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
 }
-
-
-
-// referred to as a setter
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-// this was created to read the content of the element and then store it into x
-// referred to as a getter
-// var x = document.querySelector('#score-0').textContent;
-//
---
+++ practice/script.js
@@ -0,0 +1,13 @@
+var x, y;
+x = 10;
+y = 15;
+
+function easyCalc(){
+  if (x + y === 25) {
+    console.log('You are Correct!');
+  } else {
+    console.log('Please Try Again');
+  }
+}
+
+console.log(easyCalc);
